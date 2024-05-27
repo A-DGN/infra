744eebdab1102b720d166e26bb816efdc144368b
adrien.daugan <adrien.daugan@qonto.com>
2024-05-27 20:37:22 +0200

    edit

--- agents/gitlab_install/gitlab_install.sh
+++ agents/gitlab_install/gitlab_install.sh
@@ -12,18 +12,19 @@ services:
   gitlab:
     image: 'gitlab/gitlab-ce:latest'
     restart: always
-    hostname: 'gitlab.example.com'
+    hostname: '192.168.1.11'
     environment:
       GITLAB_OMNIBUS_CONFIG: |
-        external_url 'https://gitlab.example.com'
+        external_url 'http://192.168.1.11'
         gitlab_rails['gitlab_shell_ssh_port'] = 2222
+        # Ajoutez des configurations SSL ici si vous utilisez HTTPS
     ports:
       - '80:80'
       - '443:443'
       - '2222:22'
     volumes:
       - '/apps/docker/gitlab/config:/etc/gitlab'
-      - '/app/docker/gitlab/logs:/var/log/gitlab'
+      - '/apps/docker/gitlab/logs:/var/log/gitlab'
       - '/apps/docker/gitlab/data:/var/opt/gitlab'
 EOF
 
